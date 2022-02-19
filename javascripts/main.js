6aa1924483a1a74546c0914c53de7d31040074fc
erblinazhegrova <erblinazhegrova@gmail.com>
2022-02-19 17:37:31 +0100

    initial commit, created achitecture for a web with some pages

---
+++ javascripts/main.js
@@ -0,0 +1,10 @@
+// navbar togglemenu start
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
